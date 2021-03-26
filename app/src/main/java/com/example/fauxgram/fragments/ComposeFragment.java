de0215f8ebea564a2e81972f70e44fdb1ec39ef8
shabazzm <shabazzm@oregonstate.edu>
2021-03-26 14:38:26 -0500

    Added walkthrough video

--- app/src/main/java/com/example/fauxgram/fragments/ComposeFragment.java
+++ app/src/main/java/com/example/fauxgram/fragments/ComposeFragment.java
@@ -50,14 +50,7 @@ public class ComposeFragment extends Fragment {
     private String photoFileName = "photo.jpg";
     private Button btnLogout;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public ComposeFragment() {
         // Required empty public constructor
